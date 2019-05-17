babf8edbcc794710ec899a40a963f72bcfac5ebb
Clayton Thomas <onejay@outlook.com>
2019-05-16 20:02:14 -0700

    edit by Clayton

--- src/Hello/Main.java
+++ src/Hello/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is David");
+        System.out.println("Cool David, I think I'm finally getting this VCS stuff to work - Clayton");
     }
 }
