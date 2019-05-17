a89413e8d524021abb3b34a2d55dd99987d9d612
sekijohn10 <50596878+sekijohn10@users.noreply.github.com>
2019-05-17 17:08:57 -0600

    Timothy left his mark.

--- src/Hello/Main.java
+++ src/Hello/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is David");
         System.out.println("Cool David, I think I'm finally getting this VCS stuff to work - Clayton");
+        System.out.println("Hi David! I promise I left it as clean as I found it.")
     }
 }
